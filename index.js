9514f706ea8d9e42e6f642f8c079ed6cb252d219
kacperTheCat <kacper.one.borowski@gmail.com>
2018-01-14 16:06:36 +0100

    how to return all elms?

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import moviesStorage from "./movies-storage.js";
 
 const MoviesStorage = new moviesStorage() //instancjonowanie
 //klasy MoviesStorage
-const myMovies = MoviesStorage.gett();
+const myMovies = MoviesStorage.get()
 const myMovie = MoviesStorage.get(13)
-const addMovie = MoviesStorage.setter({id: 666});
+const addMovie = MoviesStorage.set({id: 666})
 const addData = MoviesStorage.set(5, {data: "new datas"});
 const removeMovie = MoviesStorage.remove(2)
-console.log(...myMovies)//tymczasowy spread
+console.log(myMovies)//tymczasowy spread
 console.log(myMovie)// zwracanie filmu o podanym id
 console.log(addMovie)
 console.log(addData)
--- movies-storage.js
+++ movies-storage.js
@@ -1,122 +1,123 @@
-const moviesArr = [{
-        "id": 1,
-        "title": "The Shawshank Redemption",
-        "year": 1994,
-        "genre": "drama",
-        "summary": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        "seen": "T"
-    },
-    {
-        "id": 2,
-        "title": "The Godfather",
-        "year": 1972,
-        "genre": "crime",
-        "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-        "seen": "T"
-    },
-    {
-        "id": 3,
-        "title": "The Dark Knight",
-        "year": 2008,
-        "genre": "action",
-        "summary": "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-        "seen": "T"
-    },
-    {
-        "id": 5,
-        "title": "12 Angry Men",
-        "year": 1957,
-        "genre": "drama",
-        "summary": "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
-        "seen": "F"
-    },
-    {
-        "id": 8,
-        "title": "Schindler's List",
-        "year": 1993,
-        "genre": "biography",
-        "summary": "In German-occupied Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazi Germans.",
-        "seen": "F"
-    },
-    {
-        "id": 13,
-        "title": "Pulp Fiction",
-        "year": 1994,
-        "genre": "crime",
-        "summary": "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-        "seen": "T"
-    },
-    {
-        "id": 21,
-        "title": "The Good, the Bad and the Ugly",
-        "year": 1966,
-        "genre": "western",
-        "summary": "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
-        "seen": "F"
-    }
-]
+const moviesArr = [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genre: "drama",
+    summary:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    seen: "T"
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    year: 1972,
+    genre: "crime",
+    summary:
+      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    seen: "T"
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    year: 2008,
+    genre: "action",
+    summary:
+      "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    seen: "T"
+  },
+  {
+    id: 5,
+    title: "12 Angry Men",
+    year: 1957,
+    genre: "drama",
+    summary:
+      "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
+    seen: "F"
+  },
+  {
+    id: 8,
+    title: "Schindler's List",
+    year: 1993,
+    genre: "biography",
+    summary:
+      "In German-occupied Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazi Germans.",
+    seen: "F"
+  },
+  {
+    id: 13,
+    title: "Pulp Fiction",
+    year: 1994,
+    genre: "crime",
+    summary:
+      "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    seen: "T"
+  },
+  {
+    id: 21,
+    title: "The Good, the Bad and the Ugly",
+    year: 1966,
+    genre: "western",
+    summary:
+      "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
+    seen: "F"
+  }
+];
 
 export default class MoviesStorage {
-    constructor() {
-        if (localStorage.movies && Array.isArray(localStorage.movies)) {
-            JSON.parse(localStorage.movies)
-        } else {
-            localStorage.movies = JSON.stringify(moviesArr) //object -> string
-        }
+  constructor() {
+    if (localStorage.movies && Array.isArray(localStorage.movies)) {
+      JSON.parse(localStorage.movies);
+    } else {
+      localStorage.movies = JSON.stringify(moviesArr); //object -> string
+    }
+  }
+
+  get(id) {
+    const movies = JSON.parse(localStorage.movies);
+    if (arguments.length == 0) {
+
+      for(let i = 0; i< movies.length; i++){
+
+        var moviesList = movies[i]
+      }
+
+      return moviesList// do rozwiązania
+
     }
 
-    getter() {
-        const movies = JSON.parse(localStorage.movies)
-        // let moviesList = ...movies // dlaczego nie mogę zwrócić ...movies?
-        return movies
+      else if (arguments.length == 1) {
+      const movie = movies.find(movie => movie.id === id);
 
+      return movie;
     }
+  }
 
-    get(id) {
-        const movies = JSON.parse(localStorage.movies)
+  set(id, data) {
+    const movies = JSON.parse(localStorage.movies);
+    if (typeof data === "undefined") data = id;
 
-        const movie = movies.find(movie => movie.id === id)
-        return movie
-    } //zwracanie filmu o podanym id
+    if (arguments.length == 1) {
+      let newMovie = data;
+      movies.push(newMovie);
+      localStorage.setItem("movies", JSON.stringify(movies));
 
-    setter(data) {
-
-            const movies = JSON.parse(localStorage.movies)
-            movies.push(data)
-            localStorage.setItem('movies', JSON.stringify(movies))
-            return movies
-
-        }
-    set(id, data) {
-        const movies = JSON.parse(localStorage.movies)
-
-        let movie = movies.find(movie => movie.id === id)
-        let newData = {data}
-         Object.assign(movie, data)
-        return movie
-
-    }//do filmu o okrelonym id dodaje opis
-
-    remove(id) {
-        const movies = JSON.parse(localStorage.movies)
-        const movieIndex = movies.findIndex(movie => movie.id === id)
-        const newMovies = [...movies.slice(0, movieIndex), ...movies.
-            slice(movieIndex + 1)
-        ]
-        return newMovies //usuwa film o podanym id
+      return movies;
+    } else if (arguments.length == 2) {
+      let movie = movies.find(movie => movie.id === id);
+      let newData = { data };
+      Object.assign(movie, data);
+      return movie;
     }
+  }
 
-    // get() {
-    //     const movies = JSON.parse(localStorage.movies)
-
-    //     return movies
-    // }// zwraca liste filmow, ale jesli jest na poczatku, jest undefined
-    // set(data) {
-
-    //     const movies = JSON.parse(localStorage.movies)
-    //     movies.push(data)
-    //     localStorage.setItem('movies', JSON.stringify(movies))
-    //     return movies
-
-    // }//wyłączam na czas kombinowania
+  remove(id) {
+    const movies = JSON.parse(localStorage.movies);
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+    const newMovies = [
+      ...movies.slice(0, movieIndex),
+      ...movies.slice(movieIndex + 1)
+    ];
+    return newMovies; //usuwa film o podanym id
+  }
 }
